b21ceb9aa599f776a961961b4f8e0c54ef0bc51c
Siriwat Kasamwattanarote <siriwat@outlook.jp>
2015-09-29 00:59:11 +0900

    Added makefile for the project.

--- alphautils.mk
+++ alphautils.mk
@@ -5,15 +5,15 @@
 ## Release
 ProjectName            :=alphautils
 ConfigurationName      :=Release
-WorkspacePath          := "/home/stylix/webstylix/code/lib"
-ProjectPath            := "/home/stylix/webstylix/code/lib/alphautils"
+WorkspacePath          := "/home/stylix/Dropbox/MyDocument/SokendaiCourse/Researches/Workspace/code"
+ProjectPath            := "/home/stylix/Dropbox/MyDocument/SokendaiCourse/Researches/Workspace/code/lib/alphautils"
 IntermediateDirectory  :=./Release
 OutDir                 := $(IntermediateDirectory)
 CurrentFileName        :=
 CurrentFilePath        :=
 CurrentFileFullPath    :=
 User                   :=Siriwat Kasamwattanarote
-Date                   :=28/09/15
+Date                   :=29/09/15
 CodeLitePath           :="/home/stylix/.codelite"
 LinkerName             :=g++
 SharedObjectLinkerName :=g++ -shared -fPIC
@@ -77,8 +77,8 @@ $(OutputFile): $(Objects)
 	@echo "" > $(IntermediateDirectory)/.d
 	@echo $(Objects0)  > $(ObjectsFileList)
 	$(AR) $(ArchiveOutputSwitch)$(OutputFile) @$(ObjectsFileList) $(ArLibs)
-	@$(MakeDirCommand) "/home/stylix/webstylix/code/lib/.build-release"
-	@echo rebuilt > "/home/stylix/webstylix/code/lib/.build-release/alphautils"
+	@$(MakeDirCommand) "/home/stylix/Dropbox/MyDocument/SokendaiCourse/Researches/Workspace/code/.build-release"
+	@echo rebuilt > "/home/stylix/Dropbox/MyDocument/SokendaiCourse/Researches/Workspace/code/.build-release/alphautils"
 
 MakeIntermediateDirs:
 	@test -d ./Release || $(MakeDirCommand) ./Release
@@ -94,7 +94,7 @@ PreBuild:
 ## Objects
 ##
 $(IntermediateDirectory)/alphautils.cpp$(ObjectSuffix): alphautils.cpp $(IntermediateDirectory)/alphautils.cpp$(DependSuffix)
-	$(CXX) $(IncludePCH) $(SourceSwitch) "/home/stylix/webstylix/code/lib/alphautils/alphautils.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/alphautils.cpp$(ObjectSuffix) $(IncludePath)
+	$(CXX) $(IncludePCH) $(SourceSwitch) "/home/stylix/Dropbox/MyDocument/SokendaiCourse/Researches/Workspace/code/lib/alphautils/alphautils.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/alphautils.cpp$(ObjectSuffix) $(IncludePath)
 $(IntermediateDirectory)/alphautils.cpp$(DependSuffix): alphautils.cpp
 	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/alphautils.cpp$(ObjectSuffix) -MF$(IntermediateDirectory)/alphautils.cpp$(DependSuffix) -MM "alphautils.cpp"
 
@@ -102,7 +102,7 @@ $(IntermediateDirectory)/alphautils.cpp$(PreprocessSuffix): alphautils.cpp
 	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/alphautils.cpp$(PreprocessSuffix) "alphautils.cpp"
 
 $(IntermediateDirectory)/hdf5_io.cpp$(ObjectSuffix): hdf5_io.cpp $(IntermediateDirectory)/hdf5_io.cpp$(DependSuffix)
-	$(CXX) $(IncludePCH) $(SourceSwitch) "/home/stylix/webstylix/code/lib/alphautils/hdf5_io.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/hdf5_io.cpp$(ObjectSuffix) $(IncludePath)
+	$(CXX) $(IncludePCH) $(SourceSwitch) "/home/stylix/Dropbox/MyDocument/SokendaiCourse/Researches/Workspace/code/lib/alphautils/hdf5_io.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/hdf5_io.cpp$(ObjectSuffix) $(IncludePath)
 $(IntermediateDirectory)/hdf5_io.cpp$(DependSuffix): hdf5_io.cpp
 	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/hdf5_io.cpp$(ObjectSuffix) -MF$(IntermediateDirectory)/hdf5_io.cpp$(DependSuffix) -MM "hdf5_io.cpp"
 
@@ -110,7 +110,7 @@ $(IntermediateDirectory)/hdf5_io.cpp$(PreprocessSuffix): hdf5_io.cpp
 	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/hdf5_io.cpp$(PreprocessSuffix) "hdf5_io.cpp"
 
 $(IntermediateDirectory)/imtools.cpp$(ObjectSuffix): imtools.cpp $(IntermediateDirectory)/imtools.cpp$(DependSuffix)
-	$(CXX) $(IncludePCH) $(SourceSwitch) "/home/stylix/webstylix/code/lib/alphautils/imtools.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/imtools.cpp$(ObjectSuffix) $(IncludePath)
+	$(CXX) $(IncludePCH) $(SourceSwitch) "/home/stylix/Dropbox/MyDocument/SokendaiCourse/Researches/Workspace/code/lib/alphautils/imtools.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/imtools.cpp$(ObjectSuffix) $(IncludePath)
 $(IntermediateDirectory)/imtools.cpp$(DependSuffix): imtools.cpp
 	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/imtools.cpp$(ObjectSuffix) -MF$(IntermediateDirectory)/imtools.cpp$(DependSuffix) -MM "imtools.cpp"
 
@@ -118,7 +118,7 @@ $(IntermediateDirectory)/imtools.cpp$(PreprocessSuffix): imtools.cpp
 	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/imtools.cpp$(PreprocessSuffix) "imtools.cpp"
 
 $(IntermediateDirectory)/lapwrap.cpp$(ObjectSuffix): lapwrap.cpp $(IntermediateDirectory)/lapwrap.cpp$(DependSuffix)
-	$(CXX) $(IncludePCH) $(SourceSwitch) "/home/stylix/webstylix/code/lib/alphautils/lapwrap.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/lapwrap.cpp$(ObjectSuffix) $(IncludePath)
+	$(CXX) $(IncludePCH) $(SourceSwitch) "/home/stylix/Dropbox/MyDocument/SokendaiCourse/Researches/Workspace/code/lib/alphautils/lapwrap.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/lapwrap.cpp$(ObjectSuffix) $(IncludePath)
 $(IntermediateDirectory)/lapwrap.cpp$(DependSuffix): lapwrap.cpp
 	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/lapwrap.cpp$(ObjectSuffix) -MF$(IntermediateDirectory)/lapwrap.cpp$(DependSuffix) -MM "lapwrap.cpp"
 
@@ -126,7 +126,7 @@ $(IntermediateDirectory)/lapwrap.cpp$(PreprocessSuffix): lapwrap.cpp
 	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/lapwrap.cpp$(PreprocessSuffix) "lapwrap.cpp"
 
 $(IntermediateDirectory)/linear_tree.cpp$(ObjectSuffix): linear_tree.cpp $(IntermediateDirectory)/linear_tree.cpp$(DependSuffix)
-	$(CXX) $(IncludePCH) $(SourceSwitch) "/home/stylix/webstylix/code/lib/alphautils/linear_tree.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/linear_tree.cpp$(ObjectSuffix) $(IncludePath)
+	$(CXX) $(IncludePCH) $(SourceSwitch) "/home/stylix/Dropbox/MyDocument/SokendaiCourse/Researches/Workspace/code/lib/alphautils/linear_tree.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/linear_tree.cpp$(ObjectSuffix) $(IncludePath)
 $(IntermediateDirectory)/linear_tree.cpp$(DependSuffix): linear_tree.cpp
 	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/linear_tree.cpp$(ObjectSuffix) -MF$(IntermediateDirectory)/linear_tree.cpp$(DependSuffix) -MM "linear_tree.cpp"
 
@@ -134,7 +134,7 @@ $(IntermediateDirectory)/linear_tree.cpp$(PreprocessSuffix): linear_tree.cpp
 	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/linear_tree.cpp$(PreprocessSuffix) "linear_tree.cpp"
 
 $(IntermediateDirectory)/report.cpp$(ObjectSuffix): report.cpp $(IntermediateDirectory)/report.cpp$(DependSuffix)
-	$(CXX) $(IncludePCH) $(SourceSwitch) "/home/stylix/webstylix/code/lib/alphautils/report.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/report.cpp$(ObjectSuffix) $(IncludePath)
+	$(CXX) $(IncludePCH) $(SourceSwitch) "/home/stylix/Dropbox/MyDocument/SokendaiCourse/Researches/Workspace/code/lib/alphautils/report.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/report.cpp$(ObjectSuffix) $(IncludePath)
 $(IntermediateDirectory)/report.cpp$(DependSuffix): report.cpp
 	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/report.cpp$(ObjectSuffix) -MF$(IntermediateDirectory)/report.cpp$(DependSuffix) -MM "report.cpp"
 
@@ -142,7 +142,7 @@ $(IntermediateDirectory)/report.cpp$(PreprocessSuffix): report.cpp
 	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/report.cpp$(PreprocessSuffix) "report.cpp"
 
 $(IntermediateDirectory)/tsp.cpp$(ObjectSuffix): tsp.cpp $(IntermediateDirectory)/tsp.cpp$(DependSuffix)
-	$(CXX) $(IncludePCH) $(SourceSwitch) "/home/stylix/webstylix/code/lib/alphautils/tsp.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/tsp.cpp$(ObjectSuffix) $(IncludePath)
+	$(CXX) $(IncludePCH) $(SourceSwitch) "/home/stylix/Dropbox/MyDocument/SokendaiCourse/Researches/Workspace/code/lib/alphautils/tsp.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/tsp.cpp$(ObjectSuffix) $(IncludePath)
 $(IntermediateDirectory)/tsp.cpp$(DependSuffix): tsp.cpp
 	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/tsp.cpp$(ObjectSuffix) -MF$(IntermediateDirectory)/tsp.cpp$(DependSuffix) -MM "tsp.cpp"
 
